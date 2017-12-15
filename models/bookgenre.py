e78cfc785232eea76a104e898e709930e3cb616b
Çağrı Emre AKIN <cagriemreakin@gmail.com>
2017-12-15 23:45:04 +0300

    Add files via upload
    BookGenre fist commit

---
+++ models/bookgenre.py
@@ -0,0 +1,15 @@
+from odoo import models, fields,api
+import re
+
+class BookGenre(models.Model):
+    
+    _name='about.genre'
+    name = fields.Char('Genre',required=True)
+    genre_id = fields.Many2one('about.book',  select=True,string="Book Genre")
+    _sql_constraints = [('name_uniq', 'unique (name)','Genre already exists!')]
+    
+    @api.constrains('name')
+    def is_valid_name(self):
+            if not re.match("^[A-Za-z]*$",self.name):
+                raise models.ValidationError('Genre should be alphabetic !')
+    
