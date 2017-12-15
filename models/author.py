a72699eca792ad6e1a668e404f7ea2cb16ccac1d
Çağrı Emre AKIN <cagriemreakin@gmail.com>
2017-12-15 12:28:24 +0300

    Add files via upload
    is_book_author is visible. _name : about.author is replaced with res.partner fixed the problem

--- models/author.py
+++ models/author.py
@@ -1,7 +1,9 @@
-from odoo import models, fields,api
+from odoo import models, fields,osv
 
 class Author(models.Model):
     _inherit= 'res.partner'
-    _name="about.author"
-    is_book_author= fields.Boolean('Is Book Author',default=False)
-    res_partner_id = fields.Many2one('res.partner', string="ResPartner")
+    _name="res.partner"
+    book_id = fields.Many2many('about.book',string='Books')
+    #res_partner_id = fields.Many2one('res.partner', string="ResPartner")
+    is_book_author= fields.Boolean('Is Book Author',required=True,default=False)
+
