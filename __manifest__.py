c8ffc2956267a6f6cf45e5c203d7000508486da7
Çağrı Emre AKIN <cagriemreakin@gmail.com>
2017-12-16 13:23:13 +0300

    Add files via upload
    data files are added

--- __manifest__.py
+++ __manifest__.py
@@ -7,5 +7,20 @@
          'views/author.xml',
          'views/genre.xml',
          'views/language.xml',
-         'views/publisher.xml'],
+         'views/publisher.xml',
+         'data/genre_data.xml',
+         'data/author_data.xml',
+         'data/language_data.xml'],
+'description': """
+        Book module to get information:
+            - Author
+            - Publisher
+            - Book Genre
+            - Language
+    """,
+
+'author': "Cagri Emre Akin",
+'version': '1.0',
+    
+    
 }
